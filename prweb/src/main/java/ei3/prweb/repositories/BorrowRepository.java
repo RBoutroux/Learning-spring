ec3b5a0a775a7a37daaa3f78b1fc2340d9c9c5c6
RBoutroux <remibtrx@gmail.com>
2024-12-04 16:34:32 +0100

    [feat] Creating repositories for each entity

---
+++ prweb/src/main/java/ei3/prweb/repositories/BorrowRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ei3.prweb.repositories;
+
+import ei3.prweb.items.Borrow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author remib
+ */
+@Repository
+public interface BorrowRepository  extends JpaRepository<Borrow, Integer> {
+    
+}
---
+++ prweb/src/main/java/ei3/prweb/repositories/PersonRepositoryCustomImpl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ei3.prweb.repositories;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author remib
+ */
+@Repository
+public class PersonRepositoryCustomImpl implements PersonRepositoryCustom {
+    
+}
