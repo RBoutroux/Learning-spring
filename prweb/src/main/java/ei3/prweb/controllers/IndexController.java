63b4de94ab196be63c85548f2e8caca3c97ba6b7
RBoutroux <remibtrx@gmail.com>
2024-12-16 10:48:39 +0100

    [feat] same but using an object to get the parameters

--- prweb/src/main/java/ei3/prweb/controllers/IndexController.java
+++ prweb/src/main/java/ei3/prweb/controllers/IndexController.java
@@ -22,12 +22,30 @@ public class IndexController {
         return new ModelAndView("index");
     }
     
+//    // Using parameters from the request
+//    @RequestMapping(value="login.do", method=RequestMethod.POST)
+//    public ModelAndView handleLoginPost(HttpServletRequest request){
+//        ModelAndView returned;
+//        
+//        String login = request.getParameter("login");
+//        String password = request.getParameter("password");
+//        
+//        if ((login != null) && (password !=null) && (login.equals("admin")) && (password.equals("admin"))){
+//            returned = new ModelAndView("users");
+//        }
+//        else {
+//            returned = new ModelAndView("index");
+//        }
+//        return returned;
+//    }
+    
+    // Using parameters from an object
     @RequestMapping(value="login.do", method=RequestMethod.POST)
-    public ModelAndView handleLoginPost(HttpServletRequest request){
+    public ModelAndView handleLoginPost(MyUser user){
         ModelAndView returned;
         
-        String login = request.getParameter("login");
-        String password = request.getParameter("password");
+        String login = user.getLogin();
+        String password = user.getPassword();
         
         if ((login != null) && (password !=null) && (login.equals("admin")) && (password.equals("admin"))){
             returned = new ModelAndView("users");
---
+++ prweb/src/main/java/ei3/prweb/controllers/MyUser.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ei3.prweb.controllers;
+
+/**
+ *
+ * @author remib
+ */
+public class MyUser {
+    private String login;
+    private String password;
+
+    public MyUser() {
+    }
+
+    public MyUser(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
